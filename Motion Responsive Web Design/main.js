6e82d2e64440207f84a094f568abadfa18a45039
Amit Maharjan <maharjanamit8@gmail.com>
2019-04-24 14:56:03 +0545

    Responsive Design Without Image Slider

--- Motion Responsive Web Design/main.js
+++ Motion Responsive Web Design/main.js
@@ -1,9 +1,16 @@
 /*For Hamburger*/
 function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-    }
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
   }
+
+  var y = document.getElementById("my-nav-icon");
+  if (y.className === "nav-icon") {
+    y.className += " responsive-nav";
+  } else {
+    y.className = "nav-icon";
+  }
+}
